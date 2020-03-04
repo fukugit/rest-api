96cbc08398b623b6991a6d2d93d9da8a33c67c9d
fukugit <fukuma1234@gmail.com>
2020-03-04 23:15:00 +0900

    Add banner for Dev

--- src/main/java/com/github/fukugit/restapi/SpringBootStarter.java
+++ src/main/java/com/github/fukugit/restapi/SpringBootStarter.java
@@ -28,6 +28,9 @@ public class SpringBootStarter {
       // リスナーの登録です。
       // https://docs.spring.io/spring-boot/docs/2.2.5.RELEASE/reference/html/spring-boot-features.html#boot-features-application-events-and-listeners
       .listeners(new SampleApplicationStartedEvent())
+      // ".profiles"は、spring.profiles.activeの値を書き換えて、下記コマンドと同じ条件で実行します。
+      // java -jar rest-api-1.0-SNAPSHOT.jar --spring.profiles.active=dev
+      //.profiles("dev")
       .run(args);
   }
 }
--- src/test/java/com/github/fukugit/restapi/endpoint/SampleTest.java
+++ src/test/java/com/github/fukugit/restapi/endpoint/SampleTest.java
@@ -1,4 +1,13 @@
 package com.github.fukugit.restapi.endpoint;
 
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SampleTest {
+  @Test
+  public void test() {
+    assertEquals(1, 1);
+  }
 }
